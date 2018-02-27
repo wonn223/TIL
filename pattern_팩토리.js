f49005e9189c3d9a51006ff37b608b2e5cf6c79a
wonn223 <jsw227@hanmail.net>
2018-02-27 14:07:43 +0900

    Update - 팩토리&&전략 패턴

--- pattern_팩토리.js
+++ pattern_팩토리.js
@@ -61,3 +61,48 @@ const Prayer = (function(){
 
 const citizen = new Prayer('Ancient', 'j');
 console.log(citizen.pray()); // 'ancient god presented'
+
+// 팩토리 패턴은 전략 패턴과 같이 사용한다.
+const TravelResult = (function(){
+    function TravelResult(durationInDays, death, cost){
+        this.durationInDays = durationInDays,
+        this.death = death,
+        this.cost = cost
+    }
+
+    return TravelResult;
+})();
+
+const SeaGoingVessel = (function(){
+    function SeaGoingVessel(){}
+
+    SeaGoingVessel.prototype.Travel = function(){
+        console.log('Traveling on a vessel');
+        return new TravelResult(6, 0.20, 100);
+    }
+
+    return SeaGoingVessel;
+})()
+
+const Horse = (function(){
+    function Horse(){}
+
+    Horse.prototype.Travel = function(){
+        console.log('Traveling with horse');
+        return new Horse(16, 0.30, 80);
+    }
+
+    return Horse;
+})();
+
+const currentMoney = 200;
+let traveler;
+
+const method = {
+    vessel : new SeaGoingVessel(),
+    horse : new Horse()
+}
+
+traveler = method.vessel;
+const travelResult = traveler.Travel(); 
+console.log(travelResult); // { durationInDays: 6, death: 0.2, cost: 100 }
